7099e0f3f644e37a00d5eb5fc621bc1972c2cf52
Hua Chen <huachen@Huas-MacBook-Pro.local>
2023-10-31 09:23:05 -0400

    init

---
+++ backend/index.js
@@ -0,0 +1,152 @@
+const data = require("./data.json");
+
+// add cors https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+const options = {
+  cors: true,
+};
+
+const io = require("socket.io")(options);
+let roleData = [];
+let players = {
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
+};
+let values1 = [];
+
+let started = false;
+let playerNameComplete = false;
+
+io.on("connection", (socket) => {
+  console.log(values1);
+  // disconnect if 2 clients connected
+  if (io.sockets.sockets.size > 5) {
+    console.log("Something went wrong! Too many players tried to connect!");
+    socket.disconnect();
+  }
+
+  // join server with socket id
+  const sockId = socket.id;
+  joinPlayers(sockId);
+
+  // get player id (1,2)
+  const id = getKeyByValue(players, sockId);
+  socket.emit("clientid", sockId);
+
+  socket.on("addToMap", (pair) => {
+    values1.push(pair);
+    // console.log("values: ");
+    // console.log(values1);
+    // console.log(Object.keys(values1).length);
+    if (values1.length >= 5) {
+      socket.emit("enoughPlayers");
+    }
+  });
+
+  socket.on("start", () => {
+    if (Object.keys(values1).length >= 5) {
+      // console.log("hi");
+      started = true;
+      values = [];
+      // Get all the client Id
+      // console.log(values1);
+      // for (const nested of nestedArray) {
+      //   const firstElement = nested[0][0];
+      //   // console.log(firstElement);
+      //   values.push(firstElement);
+      // }
+      for (let i = 0; i < Object.keys(values1).length; i++) {
+        values.push(values1[i][0]);
+        console.log("Values push:" + values1[i][0]);
+      }
+      // Shuffle
+      for (let i = values.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [values[i], values[j]] = [values[j], values[i]];
+      }
+
+      let counter1 = 0;
+      // Set the client id
+      for (const role in data.player_roles) {
+        const roleData = data.player_roles[role];
+        roleData.Client_id = values[counter1];
+        counter1++;
+      }
+
+      let counter5 = 0;
+      // Set the player name
+      for (const role in data.player_roles) {
+        const roleData = data.player_roles[role];
+        roleData.Player_name = values[0][counter5];
+        counter5++;
+      }
+      // // Set the player name
+      // for (const role in data.player_roles) {
+      //   const roleData = data.player_roles[role];
+      //   roleData.Player_name = playerNames[roleData.Client_id];
+      //   // if (roleData.Client_id == playerNames[])
+      //   // if (roleData == playerId) {
+      //   //   roleData.Client_id = playerName;
+      //   // }
+      //   // counter++;
+      // }
+      playerNameComplete = true;
+      // console.log(data.player_roles);
+      io.emit("start2", data.player_roles);
+      console.log("Match started");
+    }
+  });
+
+  // start the game when 5 players connect
+  // if (io.sockets.sockets.size == 5 && !started && playerName) {
+  // }
+
+  // send out the current state of the field + active id to continue game
+  if (started) {
+    console.log("Started is true");
+    //Shuffle the values array to ensure uniqueness
+    const shuffledValues = [...values];
+    for (let i = shuffledValues.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledValues[i], shuffledValues[j]] = [
+        shuffledValues[j],
+        shuffledValues[i],
+      ];
+    }
+
+    console.log(roleData);
+
+    socket.emit("continue", data.player_roles);
+  }
+
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+
+  // remove socket id from player object
+  socket.on("disconnect", () => {
+    let key = getKeyByValue(players, socket.id);
+    players[key] = "";
+  });
+});
+
+io.listen(3000);
+console.log("Sever listening on port 3000!");
+
+// add socket id to player obj
+function joinPlayers(clientId) {
+  for (const keyIdx in players) {
+    let curr = players[keyIdx];
+    if (curr == "") {
+      players[keyIdx] = clientId;
+      console.log(players);
+      return;
+    }
+  }
+}
+
+function getKeyByValue(obj, value) {
+  return Object.keys(obj).find((key) => obj[key] === value);
+}
---
+++ frontend/js/socket.js
@@ -0,0 +1,118 @@
+const socket = io("ws://localhost:3000");
+
+let clientId;
+const playerNames = {};
+const playerArr = [];
+const submitButton = document.getElementById("submitButton");
+const playerNameInput = document.getElementById("playerName");
+const startGameButton = document.getElementById("startGameButton");
+socket.on("clientid", (id) => {
+  clientId = id;
+  console.log(clientId);
+  console.log("HELLO");
+});
+submitButton.addEventListener("click", () => {
+  submitButton.style.color = "yellow";
+  const playerName = playerNameInput.value;
+  socket.emit("addToMap", [[clientId], [playerName]]);
+});
+socket.on("enoughPlayers", () => {
+  console.log("Hello");
+  startGameButton.style.display = "block";
+  // Add a click event listener to the "Start Game" button
+  document
+    .getElementById("startGameButton")
+    .addEventListener("click", function () {
+      socket.emit("start");
+    });
+});
+
+// get the first active player id and set up html for game
+startGameButton.addEventListener("click", () => {
+  // Handle the game start logic here
+  // You can emit an event to the server to signal the start of the game
+
+  socket.on("start2", (assignedRoles) => {
+    let YourRole = "";
+    for (const role in assignedRoles) {
+      if (assignedRoles[role].Client_id == clientId) {
+        console.log("You are " + role);
+        YourRole = role;
+      }
+    }
+
+    // activeId = startId;
+
+    document.getElementById("current-turn").classList.remove("hide");
+    document.getElementById("clientId").innerHTML = YourRole;
+  });
+});
+
+// get the active player and get field state
+socket.on("continue", (assignedRoles) => {
+  // activeId = active;
+  // for (let x = 0; x < field.length; x++) {
+  //   for (let y = 0; y < field.length; y++) {
+  //     setField(x, y, field[y][x]);
+  //   }
+  // }
+  // document.getElementById("current-turn").classList.remove("hide");
+  // document.getElementById("clientId").innerHTML =
+  //   clientId == activeId ? "your" : "not your";
+  // console.log("You are " + assignedRoles[clientId]);
+});
+
+// // update field with turn information
+// socket.on("turn", (turn) => {
+//   const { x, y, next } = turn;
+//   setField(x, y, activeId);
+
+//   activeId = next;
+//   document.getElementById("clientId").innerHTML =
+//     clientId == activeId ? "your" : "not your";
+// });
+
+// // show popup with win information
+// socket.on("over", (overObj) => {
+//   winnerId = overObj["id"];
+//   if (winnerId != 0)
+//     document.getElementById("winnerId").innerHTML =
+//       clientId == winnerId ? "won" : "lost";
+//   else document.getElementById("winnerId").innerHTML = "draw";
+
+//   socket.disconnect();
+
+//   document.getElementById("popup").classList.remove("hide");
+//   document.getElementById("current-turn").classList.add("hide");
+// });
+
+// // send turn event to server
+// function turn(x, y) {
+//   if (activeId != clientId) return;
+//   if (
+//     getField(x, y).classList.contains(token[1]) ||
+//     getField(x, y).classList.contains(token[2])
+//   )
+//     return;
+//   console.log("send");
+//   socket.emit("turn", {
+//     x: x,
+//     y: y,
+//   });
+// }
+
+// // get field
+// function getField(x, y) {
+//   return document.getElementById(`x${x}y${y}`);
+// }
+
+// // update css for field
+// function setField(x, y, id) {
+//   let field = getField(x, y);
+//   field.classList.add(`${token[id]}`);
+// }
+
+// restart the game
+function restart() {
+  window.location.reload();
+}
