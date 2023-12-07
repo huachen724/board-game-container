9ebb3881688a6e9e16e66446027fb3cdc1436c74
Hua Chen <huachen724@gmail.com>
2023-12-06 20:36:12 -0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,36 @@ const options = {
 };
 
 const io = require("socket.io")(options);
-let roleData = [];
-let clientIdPlayerNamePair = [];
+const clientIdPlayerNamePair = [];
 
-let started = false;
-let playerNameComplete = false;
-let players = {
+const players = {
   1: "",
   2: "",
   3: "",
   4: "",
   5: "",
 };
+
+let currentCaptainIndex = Math.floor(Math.random() * 5); // NOTE: 5 is hardcoded here
 let sawOtherRolesCounter = 0;
+
+// MID STATE VARIABLES
+let mission_number = 0;
+let total_succeeds = 0;
+let total_fails = 0;
+let captain = "";
+
+let CHOSEN_PLAYERS;
+
+// Vote
+let choiceCounter = 0;
+let playerVoteChoiceLength = 0;
+
+// Mission
+let containsFail_m = false;
+let playerVoteChoiceLength_m = 0;
+
+let tempCounter = 0;
 
 io.on("connection", (socket) => {
   if (io.sockets.sockets.size > 5) {
@@ -34,6 +51,7 @@ io.on("connection", (socket) => {
 
   // pair = [clientID, playerName]
   socket.on("submitName", (pair) => {
+    console.log("submitName handler");
     clientIdPlayerNamePair.push(pair);
     console.log(clientIdPlayerNamePair);
     if (clientIdPlayerNamePair.length >= 5) {
@@ -44,7 +62,7 @@ io.on("connection", (socket) => {
 
   socket.on("start", () => {
     // io.to("room 1").emit("hua");
-    console.log("Got to start stage");
+    console.log("Got to start");
     if (Object.keys(clientIdPlayerNamePair).length >= 5) {
       // console.log("hi");
       started = true;
@@ -75,7 +93,6 @@ io.on("connection", (socket) => {
       console.log("Match started");
       console.log("let assigned roles know other roles");
 
-      let knownRolesBasedOnClientId = [];
       console.log(data.player_roles);
       console.log(data.player_roles.Morgana.Player_name);
       io.emit("otherRoles", [
@@ -114,45 +131,198 @@ io.on("connection", (socket) => {
   });
 
   socket.on("saw-other-roles", () => {
+    console.log("saw-other-roles handler");
     console.log("do ig et here?");
     sawOtherRolesCounter++;
     console.log(sawOtherRolesCounter);
     if (sawOtherRolesCounter === 5) {
       io.emit("go-to-mid-state");
+      captain = nextTurn();
     }
   });
 
-  socket.on("mid-state-start", () => {
+  socket.on("mid-state-start", (data) => {
+    console.log(`Data: ${data}`);
+    // if (data && data.toUpperCase() === "RESTART") {
+    //   tempCounter = 0;
+    //   console.log("Resetting tempCounter");
+    // }
+    tempCounter++;
+    console.log(tempCounter);
+    console.log("mid-state-start handler");
+    
+    // Setting up mid state
     console.log("Got to here");
-    io.emit("mid-state", "hi");
+
+    // Start a player's turn (randomized initially)
+    // let captainClientId = nextTurn();
+    let filteredRoles = getAllButCaptain(captain);
+    console.log(captain);
+    if (tempCounter >= 5) {
+      ResetMidStateVars();
+      mission_number++;
+      tempCounter = 0;
+      console.log("Server sending mid-state");
+      io.emit("mid-state", captain, filteredRoles); // Make ALL clients move to mid-state View
+      io.to(captain).emit("mid-state-captain", filteredRoles); // info needed: captain,
+    }
   });
 
-  function getKeyByValue(object, value) {
-    return Object.keys(object).find((key) => object[key] === value);
+  socket.on("mid-state-vote", (chosenPlayers) => {
+    console.log("mid-state-vote handler");
+    io.emit("mid-state-vote-active", chosenPlayers);
+    CHOSEN_PLAYERS = getPlayersClientId(chosenPlayers);
+    captain = nextTurn();
+    console.log("mid-state-vote");
+  });
+
+  // chosenPlayers: the playerName of clients who were voted to go on the mission
+  function getPlayersClientId(chosenPlayers) {
+    console.log(chosenPlayers);
+    const clientIds = [];
+    // Iterate through chosenPlayers and find corresponding Client_id
+    chosenPlayers.forEach((playerName) => {
+      for (const i in clientIdPlayerNamePair) {
+        if (clientIdPlayerNamePair[i].playerName === playerName) {
+          clientIds.push(clientIdPlayerNamePair[i].clientId);
+        }
+      }
+    });
+
+    return clientIds;
   }
+
+  socket.on("playerVoteChoice", (choice) => {
+    console.log("playerVoteChoice handler");
+    playerVoteChoiceLength++;
+    console.log(choice);
+    if (choice === "Yes") {
+      console.log("hello");
+      choiceCounter = choiceCounter + 1;
+      console.log("choiceCounter: " + choiceCounter);
+    } else {
+      choiceCounter -= 1;
+    }
+    console.log(`choice Counter: ${choiceCounter}`);
+
+    // Vote Succeeded
+    if (choiceCounter > 0 && playerVoteChoiceLength === 5) {
+      // total_succeeds++
+      console.log("Got to success voting");
+      io.emit("vote-succeeded");
+      console.log(CHOSEN_PLAYERS);
+      CHOSEN_PLAYERS.forEach((socket_id) => {
+        console.log("Hello");
+        console.log(socket_id);
+        io.to(socket_id).emit("mission-vote");
+      });
+
+      // socket.emit("vote-succeeded"); // FUTURE: can add round # to be incremented indicating next mission
+      // Vote Failed
+    } else if (choiceCounter <= 0 && playerVoteChoiceLength === 5) {
+      // total_fails++
+      console.log("Got to failed voting");
+      CHOSEN_PLAYERS = "";
+      io.emit("vote-failed"); // FUTURE: can add round number to be the same here, indicating repeating the mission with new captain
+      // (roundNumber, NewCaptain)
+    }
+  });
+
+  socket.on("playerMissionChoice", (choice) => {
+    console.log("playerMissionChoice handler");
+    playerVoteChoiceLength_m++;
+    if (choice === "Fail") {
+      containsFail_m = true;
+    }
+
+    if (!containsFail_m && playerVoteChoiceLength_m === 2) {
+      // NOTE: NUMBER (2) HERE DEPENDS ON MISSION
+      // mission succeeeded
+      io.emit("midgame-restart");
+      total_succeeds++;
+    } else if (containsFail_m && playerVoteChoiceLength_m === 2) {
+      total_fails++;
+      io.emit("midgame-restart");
+    }
+  });
+  // function getKeyByValue(object, value) {
+  //   return Object.keys(object).find((key) => object[key] === value);
+  // }
 
   // remove socket id from player object
   socket.on("disconnect", () => {
-    let key = getKeyByValue(players, socket.id);
-    players[key] = "";
+    // let key = getKeyByValue(players, socket.id);
+    // players[key] = "";
   });
 });
 
 io.listen(3000);
 console.log("Sever listening on port 3000!");
 
-// add socket id to player obj
-function joinPlayers(clientId) {
-  for (const keyIdx in players) {
-    let curr = players[keyIdx];
-    if (curr == "") {
-      players[keyIdx] = clientId;
-      console.log(players);
-      return;
-    }
-  }
+/**
+ * Global: currentCaptainIndex
+ *
+ * socket.on("start-round-state") {
+ *  const playerNames = Object.keys(players);
+ *
+ *  nextTurn(playerNames)
+ * }
+ */
+// Get an array of player names
+
+// Function to handle the next turn
+function nextTurn() {
+  // Get a list of all the players in the game
+  const playerNames = Object.keys(data.player_roles);
+  console.log("playerNames: " + playerNames);
+  const currentCaptainName = playerNames[currentCaptainIndex];
+
+  // Game logic for the current player's turn
+  console.log(`${currentCaptainName}'s turn.`);
+  // console.log(data.player_roles..Client_id);
+  // Move to the next player
+  currentCaptainIndex = (currentCaptainIndex + 1) % playerNames.length;
+  return data.player_roles[currentCaptainName].Client_id;
 }
 
-function getKeyByValue(obj, value) {
-  return Object.keys(obj).find((key) => obj[key] === value);
+function getAllButCaptain(clientId) {
+  const filteredData = [];
+  const rolesData = data.player_roles;
+
+  for (const role in rolesData) {
+    if (rolesData[role].Client_id !== clientId) {
+      const { Client_id, Player_name } = rolesData[role];
+      filteredData.push({ Client_id, Player_name });
+    }
+  }
+
+  return filteredData;
 }
+
+function ResetMidStateVars() {
+  CHOSEN_PLAYERS = "";
+
+  // Vote
+  choiceCounter = 0;
+  playerVoteChoiceLength = 0;
+
+  // Mission
+  containsFail_m = false;
+  playerVoteChoiceLength_m = 0;
+}
+
+// // add socket id to player obj
+// function joinPlayers(clientId) {
+//   for (const keyIdx in players) {
+//     let curr = players[keyIdx];
+//     if (curr == "") {
+//       players[keyIdx] = clientId;
+//       console.log(players);
+//       return;
+//     }
+//   }
+// }
+
+// function getKeyByValue(obj, value) {
+//   return Object.keys(obj).find((key) => obj[key] === value);
+// }
--- frontend/js/socket.js
+++ frontend/js/socket.js
@@ -71,20 +71,129 @@ socket.on("otherRoles", (otherRoles) => {
 });
 
 socket.on("go-to-mid-state", () => {
+  console.log("go-to-mid-state handler");
   document.getElementById("mid-state-button").style.display = "block";
   document
     .getElementById("mid-state-button")
     .addEventListener("click", function () {
-      socket.emit("mid-state-start");
+      socket.emit("mid-state-start", "initial"); // This is where mid-state-start starts
       document.getElementById("mid-state-button").style.display = "none";
     });
 });
 
-// get the active player and get field state
+// get field state
 socket.on("mid-state", (e) => {
+  console.log("mid-state handler");
   document.getElementById("inital-state").classList.add("hide");
   document.getElementById("mid-state").classList.remove("hide");
-  console.log(e);
+
+  // console.log(e);
+});
+
+// Only the specific client_id whose role is chosen is handled here
+socket.on("mid-state-captain", (filteredRoles) => {
+  console.log("mid-state-captain handler:)");
+  createCheckList(filteredRoles);
+  let chosenPlayers = "";
+  document.getElementById("checkListSubmitButton").classList.remove("hide");
+  document
+    .getElementById("checkListSubmitButton")
+    .addEventListener("click", function () {
+      chosenPlayers = handleSubmitCheckList();
+      socket.emit("mid-state-vote", chosenPlayers);
+      document.getElementById("playerChecklist").classList.add("hide");
+      document.getElementById("checkListSubmitButton").classList.add("hide");
+    });
+  console.log(chosenPlayers);
+});
+
+// Everyone but the captain is handled here via broadcast on the server side
+socket.on("mid-state-vote-active", (chosenPlayers) => {
+  console.log("mid-state-vote-active handler");
+  // Attach click event to the "Yes" button
+  document.getElementById("selectedPlayers").classList.remove("hide");
+  console.log("Chosen players: " + chosenPlayers);
+  // var clientIdElements = document.getElementsByClassName("clientId");
+
+  // document.getElementsByClassName("selected-players").innerHTML = chosenPlayers;
+
+  // Get all elements with the class "selected-players"
+  const selectedPlayersElements =
+    document.getElementsByClassName("selected-players");
+
+  // Iterate through the collection and set the innerHTML for each element
+  for (let i = 0; i < selectedPlayersElements.length; i++) {
+    selectedPlayersElements[i].innerHTML = chosenPlayers;
+  }
+  document.getElementById("yesVoteButton").classList.remove("hide");
+  document.getElementById("noVoteButton").classList.remove("hide");
+  document
+    .getElementById("yesVoteButton")
+    .addEventListener("click", function () {
+      socket.emit("playerVoteChoice", "Yes");
+      document.getElementById("yesVoteButton").classList.add("hide");
+      document.getElementById("noVoteButton").classList.add("hide");
+      document.getElementById("selectedPlayers").classList.add("hide");
+    });
+
+  // Attach click event to the "No" button
+  document
+    .getElementById("noVoteButton")
+    .addEventListener("click", function () {
+      socket.emit("playerVoteChoice", "No");
+      document.getElementById("yesVoteButton").classList.add("hide");
+      document.getElementById("noVoteButton").classList.add("hide");
+      document.getElementById("selectedPlayers").classList.add("hide");
+    });
+});
+
+// Selected players go on mission and ONLY they vote (for fail or succeed)
+socket.on("vote-succeeded", () => {
+  console.log("vote-succeeded handler");
+  alert("Vote succeeded. Selected players will now go on the mission..");
+});
+
+// private message to selected players to go on mission
+socket.on("mission-vote", () => {
+  console.log("mission-vote handler");
+  alert(
+    "You are on currently on the mission, are you succeeding or failing it?"
+  );
+  // Attach click event to the "Yes" button
+  document.getElementById("missionMessage").classList.remove("hide");
+
+  document.getElementById("succeedButton").classList.remove("hide");
+  document.getElementById("failButton").classList.remove("hide");
+  document
+    .getElementById("succeedButton")
+    .addEventListener("click", function () {
+      socket.emit("playerMissionChoice", "Succeed");
+      document.getElementById("succeedButton").classList.add("hide");
+      document.getElementById("failButton").classList.add("hide");
+      document.getElementById("missionMessage").classList.add("hide");
+    });
+
+  // Attach click event to the "No" button
+  document.getElementById("failButton").addEventListener("click", function () {
+    socket.emit("playerMissionChoice", "Fail");
+    document.getElementById("failButton").classList.add("hide");
+    document.getElementById("succeedButton").classList.remove("hide");
+    document.getElementById("missionMessage").classList.add("hide");
+  });
+});
+// Reset the temp counter
+socket.on("vote-failed", () => {
+  // NOTE: Remove/Reset all the old info
+  console.log("vote-failed handler");
+  clearData();
+  socket.emit("mid-state-start", "failed");
+});
+
+socket.on("midgame-restart", () => {
+  console.log("midgame-restart handler");
+  console.log("restart mid state");
+  clearData();
+  socket.emit("mid-state-start", "restart");
 });
 
 // restart the game
@@ -122,6 +231,38 @@ function displayRoles(playerRoles) {
   document.getElementById("show-other-roles").classList.remove("hide");
 }
 
-function memberList(playerRoles) {
-  var options = document.getElementById("");
+function createCheckList(rolesData) {
+  console.log("X");
+  const playerChecklistContainer = document.getElementById("playerChecklist");
+  playerChecklistContainer.classList.remove("hide");
+  rolesData.forEach((player) => {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "player";
+    checkbox.value = player.Player_name;
+
+    const label = document.createElement("label");
+    label.appendChild(document.createTextNode(player.Player_name));
+
+    playerChecklistContainer.appendChild(checkbox);
+    playerChecklistContainer.appendChild(label);
+    playerChecklistContainer.appendChild(document.createElement("br"));
+  });
+}
+
+function handleSubmitCheckList() {
+  const checkedPlayers = Array.from(
+    document.querySelectorAll('input[name="player"]:checked')
+  ).map((checkbox) => checkbox.value);
+  console.log("Checked players:", checkedPlayers);
+
+  socket.emit("checkedPlayers", checkedPlayers);
+
+  return checkedPlayers;
+}
+
+function clearData() {
+  document.getElementById("playerChecklist").innerHTML = "";
+  document.getElementById("selectedPlayers").innerHTML = "";
+  document.getElementsByClassName("selected-players").innerHTML = "";
 }
